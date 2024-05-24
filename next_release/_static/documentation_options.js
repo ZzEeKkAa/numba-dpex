8c7db938606e8d1dbb2ddde1b9a0868260017781
ZzEeKkAa <ZzEeKkAa@users.noreply.github.com>
2024-05-24 17:55:59 +0000

    chore(main): release 0.23.0 ce2c620be2a9e03888667435fae20c7931508525

---
+++ next_release/_static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '0.23.0rc2+50.gce2c620b',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
